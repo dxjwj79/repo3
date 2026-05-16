1ce69b2f69272c6a7d47eb4641e07878ad3c997f
dxjwj79 <casings_fractal7o@icloud.com>
2026-05-15 21:56:29 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ⬆️ Bump this version number every time you push an update
-const VERSION = 'v5';
+const VERSION = 'v6';
 const CACHE = 'inbox-' + VERSION;
 
 self.addEventListener('install', e => {
